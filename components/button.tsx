1ae6ba8dba94fae04928e7cf1668c1fa03bc5656
HenrikEide <Henrik225@gmail.com>
2024-08-26 17:49:10 +0200

    button neatening

--- components/button.tsx
+++ components/button.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 
 interface ButtonProps {
-  variant?: 'primary' | 'secondary' | 'outline';
-  size?: 'small' | 'medium' | 'large';
+  variant: 'primary' | 'secondary' | 'outline';
+  size: 'small' | 'medium' | 'large';
   disabled?: boolean;
   onClick?: () => void;
   children: React.ReactNode;
 }
 
-const getButtonClasses = (variant: string, size: string, disabled: boolean) => {
-  const baseClasses = 'rounded-3xl';
-
-  const variantClasses: { [key: string]: string } = {
-    primary: 'bg-red-600 text-white hover:bg-red-700 focus:ring-red-500',
-    secondary: 'bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-500',
-    outline: 'bg-transparent border border-red-500 text-white-500 hover:bg-red-600 focus:ring-red-500',
+const getButtonClasses = ({variant, size, disabled}: ButtonProps) => {
+    const baseClasses = 'rounded-3xl';
+  
+    const variantClasses = {
+      primary: 'bg-red-600 text-white hover:bg-red-700 focus:ring-red-500',
+      secondary: 'bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-500',
+      outline: 'bg-transparent border border-red-500 text-red-500 \
+                hover:bg-red-600 focus:ring-red-500',
+    }[variant];
+  
+    const sizeClasses = {
+      small: 'px-2 py-1 text-xs',
+      medium: 'px-4 py-2 text-sm',
+      large: 'px-6 py-3 text-base',
+    }[size];
+  
+    const disabledClasses = disabled
+      ? 'opacity-50 cursor-not-allowed'
+      : 'hover:shadow-md';
+  
+    return `${baseClasses} ${variantClasses} ${sizeClasses} ${disabledClasses}`;
   };
-
-  const sizeClasses: { [key: string]: string } = {
-    small: 'px-2 py-1 text-xs',
-    medium: 'px-4 py-2 text-sm',
-    large: 'px-6 py-3 text-base',
-  };
-
-  const disabledClasses = disabled
-    ? 'opacity-50 cursor-not-allowed'
-    : 'hover:shadow-md';
-
-  return `${baseClasses} ${variantClasses[variant]} ${sizeClasses[size]} ${disabledClasses}`;
-};
 
 export default function Button({
         variant = 'primary',
@@ -39,7 +40,7 @@ export default function Button({
     }: ButtonProps) {
   return (
     <button
-      className={getButtonClasses(variant, size, disabled)}
+      className={getButtonClasses({variant, size, disabled, children})}
       onClick={onClick}
       disabled={disabled}
     >
